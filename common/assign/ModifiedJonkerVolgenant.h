6100a492b1c9f2090a870bf71dda24ad5e536dde
Hug7 <56196862+hug7@users.noreply.github.com>
2024-05-30 20:01:26 +0800

    fix: 修复ModifiedJonkerVolgenant的bug

--- common/assign/ModifiedJonkerVolgenant.h
+++ common/assign/ModifiedJonkerVolgenant.h
@@ -9,16 +9,16 @@ using namespace std;
 
 class ModifiedJonkerVolgenant {
 private:
-    unsigned int rowCount{};  // 行数
-    unsigned int colCount{};  // 列数
-    int *col4row{};  // row是索引，col是索引对应的值，且值不为-1
-    int *row4col{};  // col是索引，row是索引对应的值，且值不为-1
-    double *rowDual{};  // row的对偶变量
-    double *colDual{};  // col的对偶变量
-    double gain{};
+    unsigned int rowCount;  // 行数
+    unsigned int colCount;  // 列数
+    int *col4row;  // row是索引，col是索引对应的值，且值不为-1
+    int *row4col;  // col是索引，row是索引对应的值，且值不为-1
+    double *rowDual;  // row的对偶变量
+    double *colDual;  // col的对偶变量
+    double gain;
     vector<vector<double>> cost;  // 成本矩阵
-    bool maximize{};  // 最大化
-    bool didFlip{};  // 是否行列转换
+    bool maximize;  // 最大化
+    bool didFlip;  // 是否行列转换
 public:
     ModifiedJonkerVolgenant(vector<vector<double>> &cost);
 
